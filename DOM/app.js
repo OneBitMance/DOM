fca6958339ddea6cd7459f64b652bcb85a20421f
OneBitMance <119012174+OneBitMance@users.noreply.github.com>
2023-03-07 09:37:48 +0100

    Add files via upload

---
+++ DOM/app.js
@@ -0,0 +1,18 @@
+// const childEl = document.querySelector(".paragraph");
+// const addBtn = document.querySelector(".add-elements");
+
+// const people = ["Mario", "Danilo", "Vukasin"];
+
+// addBtn.addEventListener("click", function () {
+//   const allPeople = document.querySelectorAll(".person");
+//   // allPeople = [div.person (1), div.person (2), div.person (3)];
+
+//   allPeople.forEach(person => person.remove());
+
+//   people.forEach(person => {
+//     const personEl = document.createElement("div");
+//     personEl.classList.add("child-element", "person");
+//     personEl.textContent = person;
+//     document.body.append(personEl);
+//   });
+// });
